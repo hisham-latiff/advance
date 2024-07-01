1898070dfc1c049b9b5f57ecf40297d3a210309f
hisham_latiff <hisham_latiff@reazonmy.com>
2024-07-01 15:32:52 +0800

    Define model for Employee, Department and Organization with its relative relations between models.

--- src/main/java/dev/almaa/sample_advance/graphql/advance/model/Department.java
+++ src/main/java/dev/almaa/sample_advance/graphql/advance/model/Department.java
@@ -1,2 +1,27 @@
-package dev.almaa.sample_advance.graphql.advance.model;public class Department {
+package dev.almaa.sample_advance.graphql.advance.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Integer id;
+    private String name;
+    @OneToMany(mappedBy = "department")
+    private Set<Employee> employees;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Organization organization;
 }
--- src/main/java/dev/almaa/sample_advance/graphql/advance/model/Organization.java
+++ src/main/java/dev/almaa/sample_advance/graphql/advance/model/Organization.java
@@ -1,2 +1,26 @@
-package dev.almaa.sample_advance.graphql.advance.model;public class Organization {
+package dev.almaa.sample_advance.graphql.advance.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Organization {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Integer id;
+    private String name;
+    @OneToMany(mappedBy = "organization")
+    private Set<Department> departments;
+    @OneToMany(mappedBy = "organization")
+    private Set<Employee> employees;
 }
