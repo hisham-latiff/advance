2152da9e026a44b0d3f5f6dfa7768c3846ecddf6
hisham_latiff <hisham_latiff@reazonmy.com>
2024-07-01 11:55:09 +0800

    First commit. Project startup.

---
+++ src/main/java/dev/almaa/sample_advance/graphql/advance/AdvanceApplication.java
@@ -0,0 +1,13 @@
+package dev.almaa.sample_advance.graphql.advance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/almaa/sample_advance/graphql/advance/AdvanceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.almaa.sample_advance.graphql.advance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
